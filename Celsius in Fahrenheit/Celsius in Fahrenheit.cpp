9247a6de250cf4dca2cb04424384437dca1d2b9d
Nikita Polunin (二木立・半人) <Stkchanelx@gmail.com>
2021-10-15 18:03:47 +0300

    Добавьте файлы проекта.

---
+++ Celsius in Fahrenheit/Celsius in Fahrenheit.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    double cel, fahr;
+    cout << "Please enter a Celsius volume: ";
+    cin >> cel;
+
+    fahr = 1.8 * cel + 32;
+    cout << cel << " degrees Celsius is " << fahr << " degrees Fahrenheit" << endl;
+}
