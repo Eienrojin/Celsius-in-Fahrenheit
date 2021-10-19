82af12ecf549114cf563b3a172cb00cc1b61a38f
Nikita Polunin (二木立・半人) <Stkchanelx@gmail.com>
2021-10-19 15:11:38 +0300

    Func

--- Celsius in Fahrenheit/Celsius in Fahrenheit.cpp
+++ Celsius in Fahrenheit/Celsius in Fahrenheit.cpp
@@ -1,12 +1,19 @@
 ﻿#include <iostream>
 using namespace std;
 
+double convert(double);
+
 int main()
 {
     double cel, fahr;
     cout << "Please enter a Celsius volume: ";
     cin >> cel;
 
-    fahr = 1.8 * cel + 32;
+    fahr = convert(cel);
     cout << cel << " degrees Celsius is " << fahr << " degrees Fahrenheit" << endl;
+}
+
+double convert(double cel)
+{
+    return 1.8 * cel + 32;
 }
